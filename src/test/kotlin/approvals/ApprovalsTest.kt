8dff046a3874e9551819b8d32e526ae1f4d1534c
Duncan McGregor <duncan@oneeyedmen.com>
2019-03-08 20:51:12 +0000

    Add an okeydoke example

---
+++ src/test/kotlin/approvals/ApprovalsTest.kt
@@ -0,0 +1,36 @@
+package approvals
+
+import com.oneeyedmen.okeydoke.Approver
+import com.oneeyedmen.okeydoke.Sources
+import dev.minutest.TestDescriptor
+import dev.minutest.junit.JUnit5Minutests
+import dev.minutest.rootContext
+import java.io.File
+
+
+class ApprovalsTest : JUnit5Minutests {
+
+    class Fixture(testDescriptor: TestDescriptor) {
+        val approver = Approver(testDescriptor.fullName().drop(1).joinToString(">"), Sources.`in`(File("src/test/kotlin"), this::class.java.`package`))
+    }
+
+    fun tests() = rootContext<Fixture> {
+
+        fixture { testDescriptor ->  Fixture(testDescriptor) }
+
+        test("in root") {
+            approver.assertApproved("banana")
+        }
+
+        context("context") {
+            test("in context") {
+                approver.assertApproved("kumquat")
+            }
+
+        }
+
+        after {
+            if (!approver.satisfactionChecked()) approver.assertSatisfied()
+        }
+    }
+}
